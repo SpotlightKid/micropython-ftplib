8f8afa78ff5387b2f38f78ed6819d627d46cacb6
Christopher Arndt <chris@chrisarndt.de>
2017-01-25 15:16:13 +0100

    Add missing module namespace to function calls/exceptions
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- ftpcp.py
+++ ftpcp.py
@@ -7,19 +7,23 @@ def ftpcp(source, sourcename, target, targetname='', type='I'):
     """Copy file from one FTP-instance to another."""
     if not targetname:
         targetname = sourcename
+
     type = 'TYPE ' + type
     source.voidcmd(type)
     target.voidcmd(type)
-    sourcehost, sourceport = parse227(source.sendcmd('PASV'))
+    sourcehost, sourceport = ftplib.parse227(source.sendcmd('PASV'))
     target.sendport(sourcehost, sourceport)
     # RFC 959: the user must "listen" [...] BEFORE sending the
     # transfer request.
     # So: STOR before RETR, because here the target is a "user".
     treply = target.sendcmd('STOR ' + targetname)
+
     if treply[:3] not in {'125', '150'}:
-        raise error_proto  # RFC 959
+        raise ftplib.error_proto  # RFC 959
+
     sreply = source.sendcmd('RETR ' + sourcename)
     if sreply[:3] not in {'125', '150'}:
-        raise error_proto  # RFC 959
+        raise ftplib.error_proto  # RFC 959
+
     source.voidresp()
     target.voidresp()
