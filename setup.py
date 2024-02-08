534328ec797c626ee30726759d403b72974168f3
Christopher Arndt <chris@chrisarndt.de>
2024-02-08 06:45:58 +0100

    Bump version to 0.3.0
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ module variable. The test code has been moved to a separate script and reworked
 too. The 'ftpcp' function has been moved to the 'ftpcp.py' module.
 
 A new FTP-over-TLS implementation has been added based on the present
-implementation in teh CPython standard library, but has been extensively
+implementation in the CPython standard library, but has been extensively
 reworked as well.
 
 The code has been tested under the following MicroPython ports against the FTP
@@ -21,11 +21,14 @@ server from the 'pyftpdlib' package.
 * stm32 (using W5500 ethernet module)
 * esp8266
 * esp32
+* rp2 (Raspberry Pi Pico W)
 
-For the esp8266 and esp32 port, use the specially adapted modules in the the
-`esp` sub-directory. FTP-over-SSL support for esp32 is currently in alpha
-stage and may or may not actually work, depending on your MicroPython version
-and the amount of available RAM on your board.
+For the esp8266 port, use the specially adapted modules in the the `esp`
+sub-directory.
+
+FTP-over-SSL support for esp32 is may or may not actually work, depending on
+the installed MicroPython firmware version and the amount of available RAM on
+the board used.
 
 """
 
@@ -33,7 +36,7 @@ from setuptools import setup
 
 setup(
     name='micropython-ftplib',
-    version='0.2.0',
+    version='0.3.0',
     description=__doc__.splitlines()[0],
     long_description="".join(__doc__.splitlines()[2:]),
     url='https://github.com/SpotlightKid/micropython-ftplib',
